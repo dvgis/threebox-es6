47aafebfcc6074b906b25255c4963e37864b23db
Jesus Serrano <jscastro76@users.noreply.github.com>
2020-06-15 09:22:31 +0200

    removing some objects from git

--- dist/threebox.js
+++ dist/threebox.js
@@ -30,6 +30,12 @@ Threebox.prototype = {
 		this.map.repaint = true;
 	},
 
+	/**
+	 * Threebox constructor init method
+	 * @param {mapboxgl.map} map
+	 * @param {WebGLRenderingContext} glContext
+	 * @param {defaultOptions} options
+	 */
 	init: function (map, glContext, options) {
 
 		this.map = map;
@@ -443,6 +449,7 @@ Threebox.prototype = {
 			}
 		}
 	},
+
 	//[jscastro] Custom Layers doesn't work on minzoom and maxzoom attributes, and if the layer is including labels they don't hide either on minzoom
 	setLayerZoomRange: function (layer3d, minZoomLayer, maxZoomLayer) {
 		this.map.setLayerZoomRange(layer3d, minZoomLayer, maxZoomLayer)
@@ -589,7 +596,7 @@ Threebox.prototype = {
 
 	programs: function () { return this.renderer.info.programs.length },
 
-	version: '1.0.0',
+	version: '2.0.1',
 
 }
 
@@ -892,6 +899,8 @@ module.exports = exports = {
     DEG2RAD: Math.PI / 180,
     RAD2DEG: 180 / Math.PI,
     EARTH_CIRCUMFERENCE: 40075000, // In meters
+    FOV: 0.6435011087932844,
+    TILE_SIZE: 512
 }
 },{}],5:[function(require,module,exports){
 // Type validator
@@ -1035,7 +1044,7 @@ AnimationManager.prototype = {
 		obj.mixer;
 
 		//[jscastro] if the object includes animations
-		if (obj.animations && obj.animations.length) {
+		if (obj.animations && obj.animations.length > 0) {
 
 			obj.hasDefaultAnimation = true;
 
@@ -1158,11 +1167,13 @@ AnimationManager.prototype = {
 		obj.animationMethod = null;
 
 		//[jscastro] stop animation and the queue
-		obj.stop = function () {
+		obj.stop = function (index) {
 			if (obj.mixer) {
 				obj.isPlaying = false;
 				cancelAnimationFrame(obj.animationMethod);
 			}
+			//TODO: if this is removed, it produces an error in 
+			this.animationQueue = [];
 			return this;
 		}
 
@@ -1230,7 +1241,7 @@ AnimationManager.prototype = {
 
 		//[jscastro] play default animation
 		obj.playDefault = function (options) {
-			if (obj.mixer) {
+			if (obj.mixer && obj.hasDefaultAnimation) {
 
 				var newParams = {
 					start: Date.now(),
@@ -1335,6 +1346,7 @@ AnimationManager.prototype = {
 
 				//focus on first item in queue
 				var item = object.animationQueue[i];
+				if (!item) continue;
 				var options = item.parameters;
 
 				// if an animation is past its expiration date, cull it
@@ -1355,7 +1367,6 @@ AnimationManager.prototype = {
 				if (expiring) {
 					options.expiration = false;
 					if (item.type === 'playDefault') {
-						//object.isPlaying = false;
 						object.stop();
 					} else {
 						if (options.endState) object._setObject(options.endState);
@@ -1446,134 +1457,124 @@ const defaults = {
 module.exports = exports = AnimationManager;
 },{"../Threebox.js":2,"../three.js":22,"../utils/utils.js":25,"../utils/validate.js":26}],7:[function(require,module,exports){
 var THREE = require("../three.js");
-var utils = require("../utils/utils.js");
-var ThreeboxConstants = require("../utils/constants.js");
+var utils = require("../Utils/Utils.js");
+var ThreeboxConstants = require("../Utils/constants.js");
 
 function CameraSync(map, camera, world) {
+    this.map = map;
+    this.camera = camera;
+    this.active = true;
 
-	this.map = map;
-	this.camera = camera;
-	this.active = true;
+    this.camera.matrixAutoUpdate = false;   // We're in charge of the camera now!
 
-	this.camera.matrixAutoUpdate = false;   // We're in charge of the camera now!
+    // Postion and configure the world group so we can scale it appropriately when the camera zooms
+    this.world = world || new THREE.Group();
+    this.world.position.x = this.world.position.y = ThreeboxConstants.WORLD_SIZE / 2
+    this.world.matrixAutoUpdate = false;
 
-	// Postion and configure the world group so we can scale it appropriately when the camera zooms
-	this.world = world || new THREE.Group();
-	this.world.position.x = this.world.position.y = ThreeboxConstants.WORLD_SIZE / 2
-	this.world.matrixAutoUpdate = false;
+    // set up basic camera state
+    this.state = {
+        fov: ThreeboxConstants.FOV,
+        translateCenter: new THREE.Matrix4().makeTranslation(ThreeboxConstants.WORLD_SIZE / 2, -ThreeboxConstants.WORLD_SIZE / 2, 0),
+        worldSizeRatio: ThreeboxConstants.TILE_SIZE / ThreeboxConstants.WORLD_SIZE
+    };
 
+    // Listen for move events from the map and update the Three.js camera
+    var _this = this;
+    this.map
+        .on('move', function () {
+            _this.updateCamera()
+        })
+        .on('resize', function () {
+            _this.setupCamera();
+        })
 
-	//set up basic camera state
-
-	this.state = {
-		fov: 0.6435011087932844,
-		translateCenter: new THREE.Matrix4,
-		worldSizeRatio: 512 / ThreeboxConstants.WORLD_SIZE
-	};
-
-	this.state.translateCenter.makeTranslation(ThreeboxConstants.WORLD_SIZE / 2, -ThreeboxConstants.WORLD_SIZE / 2, 0);
-
-	// Listen for move events from the map and update the Three.js camera. Some attributes only change when viewport resizes, so update those accordingly
-	var _this = this;
-
-	this.map
-		.on('move', function () {
-			_this.updateCamera()
-		})
-		.on('resize', function () {
-			_this.setupCamera();
-		})
-
-
-	this.setupCamera();
+    this.setupCamera();
 }
 
 CameraSync.prototype = {
+    setupCamera: function () {
+        const t = this.map.transform;
+        const halfFov = this.state.fov / 2;
+        const cameraToCenterDistance = 0.5 / Math.tan(halfFov) * t.height;
+        const maxPitch = t._maxPitch * Math.PI / 180;
+        const acuteAngle = Math.PI / 2 - maxPitch;
 
-	setupCamera: function () {
+        this.state.cameraToCenterDistance = cameraToCenterDistance;
+        this.state.cameraTranslateZ = new THREE.Matrix4().makeTranslation(0, 0, cameraToCenterDistance);
+        this.state.maxFurthestDistance = cameraToCenterDistance * 0.95 * (Math.cos(acuteAngle) * Math.sin(halfFov) / Math.sin(Math.max(0.01, Math.min(Math.PI - 0.01, acuteAngle - halfFov))) + 1);
 
-		var t = this.map.transform
-		const halfFov = this.state.fov / 2;
-		var cameraToCenterDistance = 0.5 / Math.tan(halfFov) * t.height;
-		const groundAngle = Math.PI / 2 + t._pitch;
+        this.updateCamera();
+    },
 
-		this.state.cameraToCenterDistance = cameraToCenterDistance;
-		this.state.cameraTranslateZ = new THREE.Matrix4().makeTranslation(0, 0, cameraToCenterDistance);
-		this.state.topHalfSurfaceDistance = Math.sin(halfFov) * cameraToCenterDistance / Math.sin(Math.PI - groundAngle - halfFov);
+    updateCamera: function (ev) {
+        if (!this.camera) {
+            console.log('nocamera')
+            return;
+        }
 
-		this.updateCamera();
-	},
+        // Furthest distance optimized by @satorbs
+        // https://github.com/satorbs/threebox/blob/master/src/Camera/CameraSync.js
+        const t = this.map.transform;
+        const halfFov = this.state.fov / 2;
+        const groundAngle = Math.PI / 2 + t._pitch;
+        const topHalfSurfaceDistance = Math.sin(halfFov) * this.state.cameraToCenterDistance / Math.sin(Math.max(0.01, Math.min(Math.PI - 0.01, Math.PI - groundAngle - halfFov)));
 
-	updateCamera: function (ev) {
+        // Calculate z distance of the farthest fragment that should be rendered.
+        const furthestDistance = Math.cos(Math.PI / 2 - t._pitch) * topHalfSurfaceDistance + this.state.cameraToCenterDistance;
 
-		if (!this.camera) {
-			console.log('nocamera')
-			return;
-		}
+        // Add a bit extra to avoid precision problems when a fragment's distance is exactly `furthestDistance`
+        const farZ = Math.min(furthestDistance * 1.01, this.state.maxFurthestDistance);
 
-		var t = this.map.transform
+        // someday @ansis set further near plane to fix precision for deckgl,so we should fix it to use mapbox-gl v1.3+ correctly
+        // https://github.com/mapbox/mapbox-gl-js/commit/5cf6e5f523611bea61dae155db19a7cb19eb825c#diff-5dddfe9d7b5b4413ee54284bc1f7966d
+        const nearZ = t.height / 50;
 
-		// Calculate z distance of the farthest fragment that should be rendered.
-		const furthestDistance = Math.cos(Math.PI / 2 - t._pitch) * this.state.topHalfSurfaceDistance + this.state.cameraToCenterDistance;
+        this.camera.projectionMatrix = utils.makePerspectiveMatrix(this.state.fov, t.width / t.height, nearZ, farZ);
 
-		// Add a bit extra to avoid precision problems when a fragment's distance is exactly `furthestDistance`
-		const farZ = furthestDistance * 1.01;
+        // Unlike the Mapbox GL JS camera, separate camera translation and rotation out into its world matrix
+        // If this is applied directly to the projection matrix, it will work OK but break raycasting
+        var cameraWorldMatrix = this.calcCameraMatrix(t._pitch, t.angle);
+        this.camera.matrixWorld.copy(cameraWorldMatrix);
 
-		this.camera.projectionMatrix = utils.makePerspectiveMatrix(this.state.fov, t.width / t.height, 1, farZ);
+        var zoomPow = t.scale * this.state.worldSizeRatio;
+        // Handle scaling and translation of objects in the map in the world's matrix transform, not the camera
+        var scale = new THREE.Matrix4;
+        var translateMap = new THREE.Matrix4;
+        var rotateMap = new THREE.Matrix4;
 
+        scale.makeScale(zoomPow, zoomPow, zoomPow);
 
-		var cameraWorldMatrix = new THREE.Matrix4();
-		var cameraTranslateZ = new THREE.Matrix4().makeTranslation(0, 0, this.state.cameraToCenterDistance);
-		var rotatePitch = new THREE.Matrix4().makeRotationX(t._pitch);
-		var rotateBearing = new THREE.Matrix4().makeRotationZ(t.angle);
+        var x = t.x || t.point.x;
+        var y = t.y || t.point.y;
+        translateMap.makeTranslation(-x, y, 0);
+        rotateMap.makeRotationZ(Math.PI);
 
-		// Unlike the Mapbox GL JS camera, separate camera translation and rotation out into its world matrix
-		// If this is applied directly to the projection matrix, it will work OK but break raycasting
-
-		cameraWorldMatrix
-			.premultiply(this.state.cameraTranslateZ)
-			.premultiply(rotatePitch)
-			.premultiply(rotateBearing)
-
-		this.camera.matrixWorld.copy(cameraWorldMatrix);
-
-
-		var zoomPow = t.scale * this.state.worldSizeRatio;
-
-		// Handle scaling and translation of objects in the map in the world's matrix transform, not the camera
-		var scale = new THREE.Matrix4;
-		var translateCenter = new THREE.Matrix4;
-		var translateMap = new THREE.Matrix4;
-		var rotateMap = new THREE.Matrix4;
-
-		scale
-			.makeScale(zoomPow, zoomPow, zoomPow);
-
-
-		var x = -this.map.transform.x || -this.map.transform.point.x;
-		var y = this.map.transform.y || this.map.transform.point.y;
-
-		translateMap
-			.makeTranslation(x, y, 0);
-
-		rotateMap
-			.makeRotationZ(Math.PI);
-
-		this.world.matrix = new THREE.Matrix4;
-		this.world.matrix
-			.premultiply(rotateMap)
-			.premultiply(this.state.translateCenter)
-			.premultiply(scale)
-			.premultiply(translateMap)
+        this.world.matrix = new THREE.Matrix4()
+            .premultiply(rotateMap)
+            .premultiply(this.state.translateCenter)
+            .premultiply(scale)
+            .premultiply(translateMap)
 
 
-		//Threebox.prototype.utils.prettyPrintMatrix(this.world.matrix.elements);
-	}
+        // utils.prettyPrintMatrix(this.camera.projectionMatrix.elements);
+    },
+
+    calcCameraMatrix(pitch, angle, trz) {
+        const t = this.map.transform;
+        const _pitch = (pitch === undefined) ? t._pitch : pitch;
+        const _angle = (angle === undefined) ? t.angle : angle;
+        const _trz = (trz === undefined) ? this.state.cameraTranslateZ : trz;
+
+        return new THREE.Matrix4()
+            .premultiply(_trz)
+            .premultiply(new THREE.Matrix4().makeRotationX(_pitch))
+            .premultiply(new THREE.Matrix4().makeRotationZ(_angle));
+    }
 }
 
 module.exports = exports = CameraSync;
-
-},{"../three.js":22,"../utils/constants.js":23,"../utils/utils.js":25}],8:[function(require,module,exports){
+},{"../Utils/Utils.js":3,"../Utils/constants.js":4,"../three.js":22}],8:[function(require,module,exports){
 /**
  * @author mrdoob / http://mrdoob.com/
  */
@@ -15808,11 +15809,11 @@ Objects.prototype = {
 
 	},
 
-	_addMethods: function (obj, staticValues) {
+	_addMethods: function (obj, isStatic) {
 
 		var root = this;
 
-		if (staticValues) {
+		if (isStatic) {
 
 		}
 
@@ -16025,7 +16026,7 @@ Objects.prototype = {
 			});
 
 			//[jscastro] add CSS2 label method 
-			obj.addLabel = function (HTMLElement, visible, bottomMargin) {
+			obj.addLabel = function (HTMLElement, visible = false, bottomMargin = 0) {
 				if (HTMLElement) {
 					//we add it to the first children to get same boxing and position
 					//obj.children[0].add(obj.drawLabel(text, height));
@@ -16231,7 +16232,7 @@ Objects.prototype = {
 
 		obj.add = function () {
 			tb.add(obj);
-			if (!staticValues) obj.set({ position: obj.coordinates });
+			if (!isStatic) obj.set({ position: obj.coordinates });
 			return obj;
 		}
 
